c7be243665efcfe4490377c65cc4640a416b69bb
Nikita Puzyrenko <nikitapfe@gmail.com>
2024-10-10 13:43:46 -0400

    simplify

--- src/App.tsx
+++ src/App.tsx
@@ -10,50 +10,53 @@ import {
 import { createConversation } from "./api/createConversation";
 import type { IConversation } from "./types";
 
+const vertexShaderSource = `
+  attribute vec2 a_position;
+  attribute vec2 a_texCoord;
+  varying vec2 v_texCoord;
+  void main() {
+    gl_Position = vec4(a_position, 0, 1);
+    v_texCoord = vec2(a_texCoord.x, 1.0 - a_texCoord.y);
+  }
+`;
+
+const fragmentShaderSource = `
+  precision mediump float;
+  uniform sampler2D u_image;
+  varying vec2 v_texCoord;
+  uniform vec3 u_keyColor;
+  uniform float u_threshold;
+  void main() {
+    vec4 color = texture2D(u_image, v_texCoord);
+    float diff = length(color.rgb - u_keyColor);
+    gl_FragColor = diff < u_threshold ? vec4(0.0) : color;
+  }
+`;
+
+const initShader = (
+  gl: WebGLRenderingContext,
+  type: number,
+  source: string,
+) => {
+  const shader = gl.createShader(type)!;
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  return shader;
+};
+
 const initWebGL = (gl: WebGLRenderingContext) => {
-  const vertexShaderSource = `
-    attribute vec2 a_position;
-    attribute vec2 a_texCoord;
-    varying vec2 v_texCoord;
-    void main() {
-      gl_Position = vec4(a_position, 0, 1);
-      v_texCoord = vec2(a_texCoord.x, 1.0 - a_texCoord.y); // Flip the y-coordinate
-    }
-  `;
-
-  const fragmentShaderSource = `
-    precision mediump float;
-    uniform sampler2D u_image;
-    varying vec2 v_texCoord;
-    uniform vec3 u_keyColor;
-    uniform float u_threshold;
-    void main() {
-      vec4 color = texture2D(u_image, v_texCoord);
-      float diff = length(color.rgb - u_keyColor);
-      if (diff < u_threshold) {
-        gl_FragColor = vec4(0.0, 0.0, 0.0, 0.0);
-      } else {
-        gl_FragColor = color;
-      }
-    }
-  `;
-
-  // Create shader program
-  const vertexShader = gl.createShader(gl.VERTEX_SHADER)!;
-  gl.shaderSource(vertexShader, vertexShaderSource);
-  gl.compileShader(vertexShader);
-
-  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER)!;
-  gl.shaderSource(fragmentShader, fragmentShaderSource);
-  gl.compileShader(fragmentShader);
-
   const program = gl.createProgram()!;
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
+  gl.attachShader(
+    program,
+    initShader(gl, gl.VERTEX_SHADER, vertexShaderSource),
+  );
+  gl.attachShader(
+    program,
+    initShader(gl, gl.FRAGMENT_SHADER, fragmentShaderSource),
+  );
   gl.linkProgram(program);
   gl.useProgram(program);
 
-  // Set up buffers
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   gl.bufferData(
@@ -70,7 +73,6 @@ const initWebGL = (gl: WebGLRenderingContext) => {
     gl.STATIC_DRAW,
   );
 
-  // Set up attributes and uniforms
   const positionLocation = gl.getAttribLocation(program, "a_position");
   const texCoordLocation = gl.getAttribLocation(program, "a_texCoord");
 
@@ -82,11 +84,6 @@ const initWebGL = (gl: WebGLRenderingContext) => {
   gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
   gl.vertexAttribPointer(texCoordLocation, 2, gl.FLOAT, false, 0, 0);
 
-  const imageLocation = gl.getUniformLocation(program, "u_image");
-  const keyColorLocation = gl.getUniformLocation(program, "u_keyColor");
-  const thresholdLocation = gl.getUniformLocation(program, "u_threshold");
-
-  // Create a texture
   const texture = gl.createTexture();
   gl.bindTexture(gl.TEXTURE_2D, texture);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
@@ -97,22 +94,22 @@ const initWebGL = (gl: WebGLRenderingContext) => {
   return {
     program,
     texture,
-    imageLocation,
-    keyColorLocation,
-    thresholdLocation,
+    imageLocation: gl.getUniformLocation(program, "u_image"),
+    keyColorLocation: gl.getUniformLocation(program, "u_keyColor"),
+    thresholdLocation: gl.getUniformLocation(program, "u_threshold"),
   };
 };
 
-export const Video = ({ id }: { id: string }) => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const glRef = useRef<WebGLRenderingContext | null>(null);
+export const Video: React.FC<{ id: string }> = ({ id }) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const videoRef = useRef<HTMLVideoElement>(null);
   const [isVideoReady, setIsVideoReady] = useState(false);
-  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const glRef = useRef<WebGLRenderingContext | null>(null);
 
   const webGLContext = useMemo(() => {
-    if (canvasRef.current) {
-      console.log("initWebGL");
-      const gl = canvasRef.current.getContext("webgl", {
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const gl = canvas.getContext("webgl", {
         premultipliedAlpha: false,
         alpha: true,
       });
@@ -129,7 +126,6 @@ export const Video = ({ id }: { id: string }) => {
     if (video) {
       const checkVideoReady = () => {
         if (video.readyState >= 2) {
-          // HAVE_CURRENT_DATA or higher
           setIsVideoReady(true);
           video.removeEventListener("canplay", checkVideoReady);
         }
@@ -140,12 +136,13 @@ export const Video = ({ id }: { id: string }) => {
   }, []);
 
   useEffect(() => {
-    console.log("isVideoReady", isVideoReady, webGLContext);
     if (!isVideoReady || !webGLContext) return;
 
     const video = videoRef.current;
     const canvas = canvasRef.current;
     const gl = glRef.current;
+    if (!video || !canvas || !gl) return;
+
     const {
       program,
       texture,
@@ -167,12 +164,7 @@ export const Video = ({ id }: { id: string }) => {
 
       lastFrameTime = currentTime;
 
-      if (
-        video &&
-        canvas &&
-        gl &&
-        video.readyState === video.HAVE_ENOUGH_DATA
-      ) {
+      if (video.readyState === video.HAVE_ENOUGH_DATA) {
         canvas.width = video.videoWidth;
         canvas.height = video.videoHeight;
         gl.viewport(0, 0, canvas.width, canvas.height);
@@ -188,7 +180,6 @@ export const Video = ({ id }: { id: string }) => {
         );
 
         gl.uniform1i(imageLocation, 0);
-        // 3 255 156
         gl.uniform3f(keyColorLocation, 3 / 255, 255 / 255, 156 / 255);
         gl.uniform1f(thresholdLocation, 0.3);
 
@@ -211,21 +202,13 @@ export const Video = ({ id }: { id: string }) => {
 
   return (
     <div
-      style={{
-        height: "13rem",
-        position: "relative",
-        // width: "100%",
-        aspectRatio: "16 / 9",
-      }}
+      style={{ height: "13rem", position: "relative", aspectRatio: "16 / 9" }}
     >
       <DailyVideo
         sessionId={id}
         type="video"
         ref={videoRef}
-        style={{
-          height: "13rem",
-          display: "none",
-        }}
+        style={{ height: "13rem", display: "none" }}
       />
       <canvas
         ref={canvasRef}
@@ -303,47 +286,13 @@ export const Call = () => {
           right: "2rem",
           zIndex: "50",
           padding: "0.25rem",
-          width: "1.5rem",
           height: "1.5rem",
         }}
       >
         {!isMicEnabled ? (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <line x1="1" y1="1" x2="23" y2="23" />
-            <path d="M9 9v3a3 3 0 0 0 5.12 2.12M15 9.34V4a3 3 0 0 0-5.94-.6" />
-            <path d="M17 16.95A7 7 0 0 1 5 12v-2m14 0v2a7 7 0 0 1-.11 1.23" />
-            <line x1="12" y1="19" x2="12" y2="23" />
-            <line x1="8" y1="23" x2="16" y2="23" />
-            <title>Mic off</title>
-          </svg>
+          'Mic is Off'
         ) : (
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z" />
-            <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
-            <line x1="12" y1="19" x2="12" y2="23" />
-            <line x1="8" y1="23" x2="16" y2="23" />
-            <title>Mic on</title>
-          </svg>
+          'Mic is On'
         )}
       </button>
       <DailyAudio />
@@ -353,17 +302,12 @@ export const Call = () => {
 
 function App() {
   const [token, setToken] = useState("");
-  const [isTokenSubmitted, setIsTokenSubmitted] = useState(false);
   const [conversation, setConversation] = useState<IConversation | null>(null);
   const [loading, setLoading] = useState(false);
   const DailyCall = useDaily();
 
-  const handleTokenSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleStartCall = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    setIsTokenSubmitted(true);
-  };
-
-  const startVideoCall = async () => {
     if (token && DailyCall) {
       setLoading(true);
       try {
@@ -371,7 +315,7 @@ function App() {
         await DailyCall.join({ url: conversation.conversation_url });
         setConversation(conversation);
       } catch (error) {
-        console.error("Failed to join the call:", error);
+        alert(`Failed to join the call. ${error}`);
       }
       setLoading(false);
     } else {
@@ -385,27 +329,31 @@ function App() {
 
   return (
     <main>
-      <form onSubmit={handleTokenSubmit}>
-        <input
-          type="text"
-          value={isTokenSubmitted ? getDisplayToken() : token}
-          onChange={(e) => setToken(e.target.value)}
-          placeholder="Enter token"
-          disabled={isTokenSubmitted}
-        />
-        <button type="submit" disabled={isTokenSubmitted}>
-          Submit Token
-        </button>
+      <form onSubmit={handleStartCall} className="token-form">
+        <label htmlFor="token">
+          Enter your Tavus API token to start, or{" "}
+          <a
+            href="https://platform.tavus.io/api-keys"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            create a new one.
+          </a>
+        </label>
+        <div>
+          <input
+            id="token"
+            type="text"
+            value={conversation ? getDisplayToken() : token}
+            onChange={(e) => setToken(e.target.value)}
+            placeholder="Enter token"
+            disabled={!!conversation}
+          />
+          <button disabled={!token || loading || !!conversation} type="submit">
+            {loading ? "Loading..." : "Start Video Call"}
+          </button>
+        </div>
       </form>
-      {isTokenSubmitted && !conversation && (
-        <button
-          onClick={startVideoCall}
-          disabled={!isTokenSubmitted || loading}
-          type="button"
-        >
-          {loading ? "Loading..." : "Start Video Call"}
-        </button>
-      )}
 
       {conversation && <Call />}
     </main>
--- src/api/createConversation.ts
+++ src/api/createConversation.ts
@@ -1,32 +1,27 @@
-import type { IConversation } from "../types";
+import type { IConversation } from '../types';
 
 export const createConversation = async (
-  token: string,
+  token: string
 ): Promise<IConversation> => {
-  try {
-    const response = await fetch("https://tavusapi.com/v2/conversations", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key": token,
+  const response = await fetch('https://tavusapi.com/v2/conversations', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'x-api-key': token,
+    },
+    body: JSON.stringify({
+      // Stock Demo Persona
+      persona_id: 'p9a95912',
+      properties: {
+        apply_greenscreen: true,
       },
-      body: JSON.stringify({
-        // Stock Demo Persona
-        persona_id: "p9a95912",
-        properties: {
-          apply_greenscreen: true,
-        },
-      }),
-    });
+    }),
+  });
 
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
-
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error:", error);
-    throw error;
+  if (!response?.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
   }
+
+  const data = await response.json();
+  return data;
 };
